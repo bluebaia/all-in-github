a9b4b29bf85a1571d4ee28d206a17d224e0cbc9e
Kelly Thompson <37467057+bluebaia@users.noreply.github.com>
2025-08-24 13:00:55 +0800

    Update config.ts

--- blog/src/config.ts
+++ blog/src/config.ts
@@ -1,8 +1,8 @@
 export const SITE = {
-  website: "https://byodian.github.io/", // replace this with your deployed domain
+  website: "https://bluebaia.github.io/", // replace this with your deployed domain
   base: '/all-in-github', // base URL for the site
-  author: "byodian",
-  profile: "https://byodian.github.io/",
+  author: "bluebaia",
+  profile: "https://bluebaia.github.io/",
   desc: "A minimal, responsive, Github Actions powered and SEO-friendly Astro blog.",
   title: "All in GitHub",
   ogImage: "astropaper-og.jpg",
@@ -15,12 +15,12 @@ export const SITE = {
   editPost: {
     enabled: true,
     text: "Edit page",
-    url: "https://github.com/byodian/all-in-github/edit/main/blog/",
+    url: "https://github.com/bluebaia/all-in-github/edit/main/blog/",
   },
   dynamicOgImage: true,
   dir: "ltr", // "rtl" | "auto"
   lang: "en", // html lang code. Set this empty and default will be "en"
   timezone: "Asia/Shanghai", // Default global timezone (IANA format) https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
   siteDescription: 'All in Github 使用 Github Issues 作为博客的内容管理系统，配合 Github Actions 实现自动化的博客生成与部署。',
-  siteReadmeLink: 'https://github.com/byodian/all-in-github#readme'
+  siteReadmeLink: 'https://github.com/bluebaia/all-in-github#readme'
 } as const;
