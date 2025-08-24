39cefe561a2ebb98ce04669d056d26069c3de3f7
Kelly Thompson <37467057+bluebaia@users.noreply.github.com>
2025-08-24 13:01:05 +0800

    Update constants.ts

--- blog/src/constants.ts
+++ blog/src/constants.ts
@@ -19,7 +19,7 @@ interface Social {
 export const SOCIALS: Social[] = [
   {
     name: "GitHub",
-    href: "https://github.com/byodian/all-in-github",
+    href: "https://github.com/bluebaia/all-in-github",
     linkTitle: `${SITE.title} on GitHub`,
     icon: IconGitHub,
   },
